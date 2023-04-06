3bd4be75aa648afdea864da56af05c3fcfc030c7
qingzzhou <345206028@qq.com>
2023-04-06 19:48:12 +0800

    配置路由

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -4,7 +4,30 @@ import VueRouter from 'vue-router'
 
 // 使用插件
 Vue.use(VueRouter)
+// 引入路由组件
+import Home from '@/views/Home'
+import Login from '@/views/Login'
+import Register from '@/views/Register'
+import Search from '@/views/Search';
 // 配置路由
-export default = new VueRouter(){
-    
-}
+export default new VueRouter({
+    // 配置路由
+    routes: [
+        {
+            path: '/home',
+            component: Home
+        },
+        {
+            path: '/login',
+            component: Login
+        },
+        {
+            path: '/search',
+            component: Search
+        },
+        {
+            path: '/register',
+            component: Register
+        },
+    ]
+})
